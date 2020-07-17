3d388450be314f57d46cfde22a08aac8f57ab1a4
irfanakyavas <akyavasi@mef.edu.tr>
2020-07-17 14:04:58 +0300

    +scripts to move jars to the project dir to make getting jars easier

---
+++ move_jars_here.bat
@@ -0,0 +1,3 @@
+copy /y "robot\build\libs\robot-1.0-SNAPSHOT-all.jar" "robot.jar"
+copy /y "client\build\libs\client-1.0-SNAPSHOT-all.jar" "client.jar"
+copy /y "server\build\libs\server-1.0-SNAPSHOT-all.jar" "server.jar"
